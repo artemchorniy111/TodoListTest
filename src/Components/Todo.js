03d7b79f055fff3cfcccbedb523f51289a1cef06
artem.chorniy111 <artem.chorniy222@gmail.com>
2021-02-04 17:54:39 +0200

    TodoList

---
+++ src/Components/Todo.js
@@ -0,0 +1,20 @@
+import { Button } from 'react-bootstrap';
+import React from 'react';
+
+export default (props) => (
+    <div style={{ display: "flex", marginBottom: "10px" }}>
+        <Button 
+            variant="outline-danger" 
+            size="sm"
+            onClick={props.onDelete}
+        >del</Button>
+        <div style={{
+            marginLeft: "5px",
+            textDecoration: props.todo.complete ? "line-through" : ""
+        }}
+            onClick={props.onComplete}
+        >
+          {props.todo.complete ? "✅" : "❎"} {props.todo.text}
+        </div>
+    </div>
+);
